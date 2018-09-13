9e29b96767c5584dea6d88f5188d415685dad9a1
Peter Litvak <peter.litvak@gmail.com>
2018-09-13 12:42:16 -0400

    Account for images without 4th band

--- vrProjector/AbstractProjection.py
+++ vrProjector/AbstractProjection.py
@@ -10,165 +10,170 @@
 # limitations under the License.
 
 
-# imports
-from PIL import Image
-import math
 import abc
-import numpy as np
-
+import math
 from multiprocessing.dummy import Pool as ThreadPool
 
+import numpy as np
+# imports
+from PIL import Image
+
+
 class AbstractProjection:
-  __metaclass__ = abc.ABCMeta
+    __metaclass__ = abc.ABCMeta
 
-  def __init__(self):
-    self.use_bilinear = False
-    pass
+    def __init__(self):
+        self.use_bilinear = False
+        pass
 
-  def set_use_bilinear(self, val):
-    self.use_bilinear = val
+    def set_use_bilinear(self, val):
+        self.use_bilinear = val
 
-  def get_pixel_from_uv(self, u, v, image):
-    x = int(self.imsize[0]*u)
-    y = int(self.imsize[1]*v)
-    x = min(x,self.imsize[0]-1)
-    y = min(y,self.imsize[1]-1)
-    pix = image[y,x]
-    return pix
+    def get_pixel_from_uv(self, u, v, image):
+        x = int(self.imsize[0] * u)
+        y = int(self.imsize[1] * v)
+        x = min(x, self.imsize[0] - 1)
+        y = min(y, self.imsize[1] - 1)
+        pix = image[y, x]
+        return pix
 
-  @staticmethod
-  def _loadImage(imageFile):
-    img = Image.open(imageFile)
-    imsize = img.size
-    parsed = Image.new("RGB", imsize, (255, 255, 255))
-    parsed.paste(img, mask=img.split()[3])
-    npimage = np.array(parsed.getdata(), np.uint8).reshape(img.size[1], img.size[0], 3)
-    return npimage, imsize
+    @staticmethod
+    def _loadImage(imageFile):
+        img = Image.open(imageFile)
+        imsize = img.size
+        parsed = Image.new("RGB", imsize, (255, 255, 255))
+        bands = img.split()
+        parsed.paste(img, mask=(bands[3] if len(bands) == 4 else None))
+        npimage = np.array(parsed.getdata(), np.uint8).reshape(img.size[1], img.size[0], 3)
+        return npimage, imsize
 
-  def loadImage(self, imageFile):
-    self.image, self.imsize = self._loadImage(imageFile)
-    self.set_angular_resolution()
+    def loadImage(self, imageFile):
+        self.image, self.imsize = self._loadImage(imageFile)
+        self.set_angular_resolution()
 
-  @staticmethod
-  def _initImage(width, height):
-    image = np.ndarray((height, width, 3), dtype=np.uint8)
-    return image
+    @staticmethod
+    def _initImage(width, height):
+        image = np.ndarray((height, width, 3), dtype=np.uint8)
+        return image
 
-  def initImage(self, width, height):
-    self.image = self._initImage(width, height)
-    self.imsize = (width, height)
-    self.set_angular_resolution()
+    def initImage(self, width, height):
+        self.image = self._initImage(width, height)
+        self.imsize = (width, height)
+        self.set_angular_resolution()
 
-  @staticmethod
-  def _saveImage(img, imgsize, destFile):
-    mode = 'RGBA'
-    arr = img.reshape(img.shape[0]*img.shape[1], img.shape[2])
-    if len(arr[0]) == 3:
-        arr = np.c_[arr, 255*np.ones((len(arr),1), np.uint8)]
-    img =  Image.frombuffer(mode, imgsize, arr.tostring(), 'raw', mode, 0, 1)
-    img.save(destFile)
+    @staticmethod
+    def _saveImage(img, imgsize, destFile):
+        mode = 'RGBA'
+        arr = img.reshape(img.shape[0] * img.shape[1], img.shape[2])
+        if len(arr[0]) == 3:
+            arr = np.c_[arr, 255 * np.ones((len(arr), 1), np.uint8)]
+        img = Image.frombuffer(mode, imgsize, arr.tostring(), 'raw', mode, 0, 1)
+        img.save(destFile)
 
-  def saveImage(self, destFile):
-    self._saveImage(self.image, self.imsize, destFile)
+    def saveImage(self, destFile):
+        self._saveImage(self.image, self.imsize, destFile)
 
-  # this isn't any faster because of the GIL on the image object
-  def reprojectToThisThreaded(self, sourceProjection, numThreads):
-    uvList = []
-    fx = float(self.imsize[0])
-    fy = float(self.imsize[1])
+    # this isn't any faster because of the GIL on the image object
+    def reprojectToThisThreaded(self, sourceProjection, numThreads):
+        uvList = []
+        fx = float(self.imsize[0])
+        fy = float(self.imsize[1])
 
-    angleList = [self.angular_position((float(i)/fx,float(j)/fy)) for i in range(self.imsize[0]) for j in range(self.imsize[1])]
+        angleList = [self.angular_position((float(i) / fx, float(j) / fy)) for i in range(self.imsize[0]) for j in
+                     range(self.imsize[1])]
 
-    poolAngles = ThreadPool(numThreads)
-    image = poolAngles.map(sourceProjection.pixel_value, angleList)
-    poolAngles.close()
-    poolAngles.join()
+        poolAngles = ThreadPool(numThreads)
+        image = poolAngles.map(sourceProjection.pixel_value, angleList)
+        poolAngles.close()
+        poolAngles.join()
 
-    idx = 0
-    for x in range(self.imsize[0]):
-      for y in range(self.imsize[1]):
-        pixel = image[idx]
-        if pixel is None:
-          print(x,y)
+        idx = 0
+        for x in range(self.imsize[0]):
+            for y in range(self.imsize[1]):
+                pixel = image[idx]
+                if pixel is None:
+                    print(x, y)
+                else:
+                    self.image[y, x] = pixel
+                idx = idx + 1
+
+    def reprojectToThis(self, sourceProjection):
+        for x in range(self.imsize[0]):
+            for y in range(self.imsize[1]):
+                u = float(x) / float(self.imsize[0])
+                v = float(y) / float(self.imsize[1])
+                theta, phi = self.angular_position((u, v))
+                if theta is None or phi is None:
+                    pixel = (0, 0, 0)
+                else:
+                    pixel = sourceProjection.pixel_value((theta, phi))
+                self.image[y, x] = pixel
+
+    def point_on_sphere(self, theta, phi):
+        r = math.cos(phi)
+        return (r * math.cos(theta), r * math.sin(theta), math.sin(phi))
+
+    def pixel_value(self, angle):
+        if self.use_bilinear:
+            return self._pixel_value_bilinear_interpolated(angle)
         else:
-          self.image[y,x] = pixel
-        idx = idx + 1
+            return self._pixel_value(angle)
 
+    @abc.abstractmethod
+    def _pixel_value(self, angle):
+        return None
 
-  def reprojectToThis(self, sourceProjection):
-    for x in range(self.imsize[0]):
-      for y in range(self.imsize[1]):
-        u = float(x)/float(self.imsize[0])
-        v = float(y)/float(self.imsize[1])
-        theta, phi = self.angular_position((u,v))
-        if theta is None or phi is None:
-          pixel = (0,0,0)
-        else:
-          pixel = sourceProjection.pixel_value((theta, phi))
-        self.image[y,x] = pixel
+    @abc.abstractmethod
+    def angular_position(self, texcoord):
+        return None
 
-  def point_on_sphere(self, theta, phi):
-    r = math.cos(phi)
-    return (r*math.cos(theta), r*math.sin(theta), math.sin(phi))
+    @abc.abstractmethod
+    def set_angular_resolution(self):
+        return None
 
-  def pixel_value(self, angle):
-    if self.use_bilinear:
-      return self._pixel_value_bilinear_interpolated(angle)
-    else:
-      return self._pixel_value(angle)
+    @staticmethod
+    def bilinear_interpolation(x, y, points):
+        '''Interpolate (x,y) from values associated with four points.
 
-  @abc.abstractmethod
-  def _pixel_value(self, angle):
-    return None
+        The four points are a list of four triplets:  (x, y, value).
+        The four points can be in any order.  They should form a rectangle.
 
-  @abc.abstractmethod
-  def angular_position(self, texcoord):
-    return None
+            >>> bilinear_interpolation(12, 5.5,
+            ...                        [(10, 4, 100),
+            ...                         (20, 4, 200),
+            ...                         (10, 6, 150),
+            ...                         (20, 6, 300)])
+            165.0
 
-  @abc.abstractmethod
-  def set_angular_resolution(self):
-    return None
+        '''
+        # See formula at:  http://en.wikipedia.org/wiki/Bilinear_interpolation
 
-  @staticmethod
-  def bilinear_interpolation(x, y, points):
-      '''Interpolate (x,y) from values associated with four points.
+        points = sorted(points)  # order points by x, then by y
+        (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = points
 
-      The four points are a list of four triplets:  (x, y, value).
-      The four points can be in any order.  They should form a rectangle.
+        if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
+            raise ValueError('points do not form a rectangle')
+        if not x1 <= x <= x2 or not y1 <= y <= y2:
+            raise ValueError('(x, y) not within the rectangle')
 
-          >>> bilinear_interpolation(12, 5.5,
-          ...                        [(10, 4, 100),
-          ...                         (20, 4, 200),
-          ...                         (10, 6, 150),
-          ...                         (20, 6, 300)])
-          165.0
+        return (q11 * (x2 - x) * (y2 - y) +
+                q21 * (x - x1) * (y2 - y) +
+                q12 * (x2 - x) * (y - y1) +
+                q22 * (x - x1) * (y - y1)
+                ) / ((x2 - x1) * (y2 - y1) + 0.0)
 
-      '''
-      # See formula at:  http://en.wikipedia.org/wiki/Bilinear_interpolation
+    def _pixel_value_bilinear_interpolated(self, angle):
+        angleeps = self.angular_resolution / 8.0
+        pixelA = self._pixel_value((angle[0] - angleeps, angle[1] - angleeps))
+        pixelB = self._pixel_value((angle[0] - angleeps, angle[1] + angleeps))
+        pixelC = self._pixel_value((angle[0] + angleeps, angle[1] - angleeps))
+        pixelD = self._pixel_value((angle[0] + angleeps, angle[1] + angleeps))
 
-      points = sorted(points)               # order points by x, then by y
-      (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = points
+        pixelR = self.bilinear_interpolation(0, 0, [(-1, -1, pixelA[0]), (-1, 1, pixelB[0]), (1, -1, pixelC[0]),
+                                                    (1, 1, pixelD[0])])
+        pixelG = self.bilinear_interpolation(0, 0, [(-1, -1, pixelA[1]), (-1, 1, pixelB[1]), (1, -1, pixelC[1]),
+                                                    (1, 1, pixelD[1])])
+        pixelB = self.bilinear_interpolation(0, 0, [(-1, -1, pixelA[2]), (-1, 1, pixelB[2]), (1, -1, pixelC[2]),
+                                                    (1, 1, pixelD[2])])
 
-      if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
-          raise ValueError('points do not form a rectangle')
-      if not x1 <= x <= x2 or not y1 <= y <= y2:
-          raise ValueError('(x, y) not within the rectangle')
-
-      return (q11 * (x2 - x) * (y2 - y) +
-              q21 * (x - x1) * (y2 - y) +
-              q12 * (x2 - x) * (y - y1) +
-              q22 * (x - x1) * (y - y1)
-             ) / ((x2 - x1) * (y2 - y1) + 0.0)
-
-  def _pixel_value_bilinear_interpolated(self, angle):
-    angleeps = self.angular_resolution/8.0
-    pixelA = self._pixel_value((angle[0]-angleeps, angle[1]-angleeps))
-    pixelB = self._pixel_value((angle[0]-angleeps, angle[1]+angleeps))
-    pixelC = self._pixel_value((angle[0]+angleeps, angle[1]-angleeps))
-    pixelD = self._pixel_value((angle[0]+angleeps, angle[1]+angleeps))
-
-    pixelR = self.bilinear_interpolation(0,0, [(-1,-1, pixelA[0]), (-1,1, pixelB[0]), (1,-1, pixelC[0]), (1,1, pixelD[0])])
-    pixelG = self.bilinear_interpolation(0,0, [(-1,-1, pixelA[1]), (-1,1, pixelB[1]), (1,-1, pixelC[1]), (1,1, pixelD[1])])
-    pixelB = self.bilinear_interpolation(0,0, [(-1,-1, pixelA[2]), (-1,1, pixelB[2]), (1,-1, pixelC[2]), (1,1, pixelD[2])])
-
-    return (int(pixelR), int(pixelG), int(pixelB))
+        return (int(pixelR), int(pixelG), int(pixelB))
